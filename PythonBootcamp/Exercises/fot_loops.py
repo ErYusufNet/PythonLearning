5c98bda81395aa66b777d868d4cef2388325a977
YusufEr <yusufer.net@hotmail.com>
2025-05-14 22:53:29 +0300

    update

---
+++ PythonBootcamp/Exercises/fot_loops.py
@@ -0,0 +1,5 @@
+# for loops = execute a block of code a fixed number of times
+#you can iterate over a range,string,sequence
+for x in reversed(range(1, 11)):
+    print(x)
+print("Happy new year!!!")
