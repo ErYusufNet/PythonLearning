ed0a9d0ed4d2c2818d342bfcb84b0f445c314fa3
YusufEr <yusufer.net@hotmail.com>
2025-05-14 22:12:59 +0300

    update

--- PythonBootcamp/while_loop.py
+++ PythonBootcamp/while_loop.py
@@ -1,8 +1,14 @@
 #While loop = execute some code while some condition remains true
 
 name = input("Enter your name: ")
+age = int(input("Enter your age: "))
 while name == "":
     print("You didn't enter a name")
     name = input("Enter your name: ")
+while age  < 0:
+    print("Age cannot be negative")
+    age = input("Enter your age: ")
 
-print(f"Hello {name}")
+
+print(f"Hello {name} end = """)
+
