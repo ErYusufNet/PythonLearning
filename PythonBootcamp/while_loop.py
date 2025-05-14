6bf5daf5b3162b4f341581e03945e32f38bfd04d
YusufEr <yusufer.net@hotmail.com>
2025-05-14 22:33:03 +0300

    update

--- PythonBootcamp/while_loop.py
+++ PythonBootcamp/while_loop.py
@@ -1,5 +1,5 @@
 #While loop = execute some code while some condition remains true
-
+'''
 name = input("Enter your name: ")
 age = int(input("Enter your age: "))
 while name == "":
@@ -10,5 +10,20 @@ while age  < 0:
     age = input("Enter your age: ")
 
 
-print(f"Hello {name} end = """)
+print(f"Hello {name}", end = "")
+print(f"Hello {age} ")
+
+food = input("Enter a food you like (q to quit): ")
+while not food == "q":
+    print(f"you like {food} ")
+    food = input("Enter another food you like (q to quit): ")
+print("bye") '''
+
+num = int(input("enter a number between 1-10: "))
+while num < 1 or num > 10:
+
+    print(f"{num} is not valid")
+    num = int(input("enter a number between 1-10: "))
+print(f"your number is{num}")
+
 
