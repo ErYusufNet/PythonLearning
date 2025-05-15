3e3837719e83552ec90a9cae89ba2b37ce3bdc8c
YusufEr <yusufer.net@hotmail.com>
2025-05-15 09:53:56 +0300

    update

--- PythonBootcamp/timer.py
+++ PythonBootcamp/timer.py
@@ -1,7 +1,11 @@
 import time
+'''
 my_time = int(input("enter the time in seconds: "))
 
 for x in range(0, my_time):
     print(x)
     time.sleep(1)
-print("Hey wake up")
+print("Hey wake up") '''
+
+time = int(input("enter time in seconds: "))
+for x in range(time, -1, -1):
