f7ee3b5225e61aac35f804ac33dba6021ebc5156
YusufEr <yusufer.net@hotmail.com>
2025-05-15 10:29:13 +0300

    update

--- PythonBootcamp/timer.py
+++ PythonBootcamp/timer.py
@@ -5,7 +5,42 @@ my_time = int(input("enter the time in seconds: "))
 for x in range(0, my_time):
     print(x)
     time.sleep(1)
-print("Hey wake up") '''
+print("Hey wake up") 
 
-time = int(input("enter time in seconds: "))
-for x in range(time, -1, -1):
+sec = int(input("enter time in seconds: "))
+for x in range(sec, -1, -1):
+    print(x)
+    time.sleep(1)
+print("Hey wake up")
+
+text = input("enter text: ")
+
+for char in text:
+    print(char, end="", flush=True)
+    time.sleep(1)
+print("\ndone!")'''
+text = input("enter word: ")
+vowels="aeiouAEIOU"
+vowels_count = 0
+count = 0
+vowels_characters =""
+
+for char in text :
+    print(char, end="", flush=True)
+    time.sleep(1)
+    count += 1
+    if char in vowels:
+        vowels_count += 1
+        vowels_characters += char
+print(f"\ntotal characters: {count}")
+print(f"number of vowels: {vowels_count}")
+
+
+if vowels_count == 0:
+    print("no vowels")
+else:
+    print(f"Vowels found: {vowels_count}")
+
+
+
+
