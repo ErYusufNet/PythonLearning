07f9662ce9e7357fc937bd9b9dd4bffdb3e29835
YusufEr <yusufer.net@hotmail.com>
2025-05-16 14:29:33 +0300

    update

---
+++ PythonBootcamp/set.py
@@ -0,0 +1,5 @@
+#set = {} sırasız ve değiştirilemez (ancak eleman ekleme/silme yapılabilir).
+# #Kopya elemanlara izin vermez.
+fruits = {"apple", "orange", "banana", "cherry"}
+for fruit in fruits:
+    print(fruit)
