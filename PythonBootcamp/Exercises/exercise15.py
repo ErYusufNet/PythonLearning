d41e7768944583f9767a4e7eed45d9c461ea6aa9
YusufEr <yusufer.net@hotmail.com>
2025-05-17 21:43:29 +0300

    update

--- PythonBootcamp/Exercises/exercise15.py
+++ PythonBootcamp/Exercises/exercise15.py
@@ -1,1 +1,8 @@
-desert_name= input("Enter desert name: ")
+#2d collections exercise
+seats = ((1, 2, "X"),
+         (4, 5, 6),
+         ("X", 8, 9))
+for seat in seats:
+    for seat1 in seat:
+        print(seat1, end= " ")
+    print()
