b2b27b56eb567ede577b937b53d869363fe6be9e
YusufEr <yusufer.net@hotmail.com>
2025-05-17 22:20:07 +0300

    update

--- PythonBootcamp/Exercises/exercise17.py
+++ PythonBootcamp/Exercises/exercise17.py
@@ -1,1 +1,16 @@
 #Python quiz game
+Questions = ("What is the capital city of finland?:",
+             "What is the capital city of france?:",
+             "What is the capital city of germany?:",
+             "What is the capital city of turkey?:",
+             "What is the capital city of italy"
+
+
+                                                )
+options = (("A:Istanbul", "B:Paris", "C:Helsinki"),
+           ("A:Istanbul", "B:Paris", "C:Berlin"),
+           ("A:Istanbul", "B:Paris", "C:Berlin"),
+           ("A:Istanbul", "B:Paris", "C:Berlin"),
+           ("A:Istanbul", "B:Roma", "C:Berlin"))
+answers = ()
+guesses = ()
