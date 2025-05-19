d4cb6baa4dccb13ca408ba25ae5e26f6d0335d1e
YusufEr <yusufer.net@hotmail.com>
2025-05-19 15:10:51 +0300

    update

---
+++ PythonBootcamp/functions.py
@@ -0,0 +1,8 @@
+#function a block of reusable code
+#Place () after the function name to invoke it
+def happy_birthay():
+    print("Happy Birthay.")
+    print("How old are you now")
+happy_birthay()
+happy_birthay()
+happy_birthay()
