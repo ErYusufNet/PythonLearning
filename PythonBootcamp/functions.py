c33797d765032cad66109d6319053ce8ca515583
YusufEr <yusufer.net@hotmail.com>
2025-05-19 20:17:42 +0300

    update

--- PythonBootcamp/functions.py
+++ PythonBootcamp/functions.py
@@ -1,8 +1,11 @@
 #function a block of reusable code
 #Place () after the function name to invoke it
-def happy_birthay():
-    print("Happy Birthay.")
-    print("How old are you now")
-happy_birthay()
-happy_birthay()
-happy_birthay()
+def happy_birthay(name, age):
+    print(f"Happy Birthay {name}")
+    print(f"You are {age} years old.")
+happy_birthay("Yusuf", 30)
+
+def greet(name, city):
+    print(f"Hello, {name}! Welcome from {city}")
+    print("We hope you enjoy your stay.")
+greet("Yusuf", "Helsinki")
